5e58a711818508a1d4b43fb47bd8720a255758d4
Alexander Sabanov <alexsab@mail.ru>
2023-06-22 16:08:06 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import path from "path";
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   base: '',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        error: path.resolve(__dirname, '404.html')
+      }
+    }
+  }
 })
